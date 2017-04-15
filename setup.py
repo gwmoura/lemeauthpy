7209abb0e62db768b60b322eff90a8853d247b5c
George Moura <gwmoura@gmail.com>
2017-04-15 01:02:06 -0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+
 PACKAGE = "lemeauth"
-NAME = "Leme Auth"
+NAME = "lemeauth"
 DESCRIPTION = "Package to use Leme API to authenticate"
 AUTHOR = "George Moura"
 AUTHOR_EMAIL = "gwmoura@gcodetec.com"
@@ -10,16 +22,12 @@ setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README.rst"),
+    long_description=README,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="BSD",
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
-    package_data=find_package_data(
-   PACKAGE,
-   only_in_packages=False
-   ),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
